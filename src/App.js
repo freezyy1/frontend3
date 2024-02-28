de934e0bab2b76d22beeb33a2d44d7392010fb99
freezyy <dmitrij.satyukov@bk.ru>
2024-02-28 11:09:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import UserForm from './Components/UserForm'
+function App() {
+  return (
+    <>
+      <div className='userform'>
+        <UserForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
